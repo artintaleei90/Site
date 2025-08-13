947821e68d7f6d51b016beb5f8ccc3472abe8052
artintaleei90 <artintaleei@gmail.com>
2025-08-14 00:52:40 +0330

    Update app.py

--- app.py
+++ app.py
@@ -73,5 +73,7 @@ def order():
 
     return f"✅ سفارش ثبت شد و فاکتور برای مدیر ارسال شد!"
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
